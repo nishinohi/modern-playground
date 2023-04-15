4a5176f97c4003cfbfd545607fd74b32c72acdc1
nishinohi <tomoya.nishi@azone-japan.co.jp>
2023-04-15 15:58:58 +0900

    add link

--- components/TipTap.tsx
+++ components/TipTap.tsx
@@ -1,18 +1,24 @@
 import Highlight from '@tiptap/extension-highlight'
-import { BubbleMenu, EditorContent, FloatingMenu, useEditor } from '@tiptap/react'
+import Link from '@tiptap/extension-link'
+import { BubbleMenu, BubbleMenuProps, EditorContent, FloatingMenu, isTextSelection, useEditor } from '@tiptap/react'
 
 import StarterKit from '@tiptap/starter-kit'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { BiBold, BiCodeBlock, BiItalic } from 'react-icons/bi'
 import { FaListOl, FaListUl } from 'react-icons/fa'
 import { GoCode } from 'react-icons/go'
 import { IoIosQuote } from 'react-icons/io'
+import { IoLink } from 'react-icons/io5'
 import { MdOutlineFormatClear, MdStrikethroughS } from 'react-icons/md'
-import { RiMarkPenFill } from 'react-icons/ri'
+import { RiMarkPenFill, RiSeparator } from 'react-icons/ri'
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit, Highlight.configure({ multicolor: true })],
+    extensions: [
+      StarterKit, //
+      Highlight.configure({ multicolor: true }), //
+      Link.configure({ openOnClick: false }), //
+    ],
     content: `
       <p>
         This is an example of a Medium-like editor. Enter a new line and some buttons will appear.
@@ -29,6 +35,53 @@ const Tiptap = () => {
 
   const [isEditable, setIsEditable] = useState(true)
 
+  const setLink = useCallback(() => {
+    if (!editor) return
+    const previousUrl = editor.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor.chain().focus().extendMarkRange('link').unsetLink().run()
+
+      return
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run()
+  }, [editor])
+
+  const shuldShow = useCallback<NonNullable<BubbleMenuProps['shouldShow']>>(({ editor, view, state, from, to }) => {
+    const { doc, selection } = state
+    const { empty } = selection
+
+    // Sometime check for `empty` is not enough.
+    // Doubleclick an empty paragraph returns a node size of 2.
+    // So we check also for an empty text size.
+    const isEmptyTextBlock = !doc.textBetween(from, to).length && isTextSelection(state.selection)
+    const hasEditorFocus = view.hasFocus()
+
+    if (!hasEditorFocus || empty || isEmptyTextBlock || !editor.isEditable || editor.isActive('horizontalRule'))
+      return false
+
+    return true
+  }, [])
+
+  const shuldShowLink = useCallback<NonNullable<BubbleMenuProps['shouldShow']>>(({ editor, view, state, from, to }) => {
+    const { doc } = state
+
+    // Sometime check for `empty` is not enough.
+    // Doubleclick an empty paragraph returns a node size of 2.
+    // So we check also for an empty text size.
+    const isEmptyTextBlock = !doc.textBetween(from, to).length && isTextSelection(state.selection)
+    return isEmptyTextBlock && editor.isActive('link')
+  }, [])
+
   useEffect(() => {
     if (editor) {
       editor.setEditable(isEditable)
@@ -42,53 +95,73 @@ const Tiptap = () => {
         Editable
       </div>
       {editor && (
-        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-          <div className="flex h-7 items-center space-x-2">
-            <button
-              onClick={() => editor.chain().focus().toggleBold().run()}
-              className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
+        <>
+          <BubbleMenu
+            pluginKey={'link'}
+            editor={editor}
+            shouldShow={shuldShowLink}
+            tippyOptions={{ duration: 100, placement: 'bottom' }}
+          >
+            <a target={'_blank'} href={editor.getAttributes('link').href} rel="noreferrer">
+              {editor.getAttributes('link').href}
+            </a>
+          </BubbleMenu>
+          <BubbleMenu pluginKey={'test'} editor={editor} shouldShow={shuldShow} tippyOptions={{ duration: 100 }}>
+            <div className="flex h-7 items-center space-x-2">
+              <button
+                onClick={() => editor.chain().focus().toggleBold().run()}
+                className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
             ${editor.isActive('bold') ? 'bg-stone-200' : 'bg-white'}`}
-            >
-              <BiBold className={'fill-stone-600'} />
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleItalic().run()}
-              className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
+              >
+                <BiBold className={'fill-stone-600'} />
+              </button>
+              <button
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+                className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
             ${editor.isActive('italic') ? 'bg-stone-200' : 'bg-white'}`}
-            >
-              <BiItalic className={'fill-stone-600'} />
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleStrike().run()}
-              className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
+              >
+                <BiItalic className={'fill-stone-600'} />
+              </button>
+              <button
+                onClick={setLink}
+                className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
+            ${editor.isActive('link') ? 'bg-stone-200' : 'bg-white'}`}
+              >
+                <IoLink className={'fill-stone-600'} />
+              </button>
+              <button
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+                className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
             ${editor.isActive('strike') ? 'bg-stone-200' : 'bg-white'}`}
-            >
-              <MdStrikethroughS className={'fill-stone-600'} />
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleCode().run()}
-              className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
+              >
+                <MdStrikethroughS className={'fill-stone-600'} />
+              </button>
+              <button
+                onClick={() => editor.chain().focus().toggleCode().run()}
+                className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
             ${editor.isActive('code') ? 'bg-stone-200' : 'bg-white'}`}
-            >
-              <GoCode className={'fill-stone-600'} />
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleHighlight({ color: '#fde68a' }).run()}
-              className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
+              >
+                <GoCode className={'fill-stone-600'} />
+              </button>
+              <button
+                onClick={() => editor.chain().focus().toggleHighlight({ color: '#fde68a' }).run()}
+                className={`h-full rounded-md border border-solid border-stone-500 px-1 font-bold text-stone-600
             ${editor.isActive('highlight') ? 'bg-stone-200' : 'bg-white'}`}
-            >
-              <RiMarkPenFill className={'fill-stone-600'} />
-            </button>
-            <button
-              onClick={() => editor.chain().focus().clearNodes().unsetAllMarks().run()}
-              className={`h-full rounded-md border border-solid border-stone-500 bg-white px-1 font-bold text-stone-600`}
-            >
-              <MdOutlineFormatClear className={'fill-stone-600'} />
-            </button>
-          </div>
-        </BubbleMenu>
+              >
+                <RiMarkPenFill className={'fill-stone-600'} />
+              </button>
+              <button
+                onClick={() => editor.chain().focus().clearNodes().unsetAllMarks().run()}
+                className={`h-full rounded-md border border-solid border-stone-500 bg-white px-1 font-bold text-stone-600`}
+              >
+                <MdOutlineFormatClear className={'fill-stone-600'} />
+              </button>
+            </div>
+          </BubbleMenu>
+        </>
       )}
       {editor && (
+        // <FloatingMenu editor={editor} shouldShow={shuldShow} tippyOptions={{ duration: 100, placement: 'left' }}>
         <FloatingMenu editor={editor} tippyOptions={{ duration: 100, placement: 'left' }}>
           <div className="flex h-7 items-center space-x-2">
             <button
@@ -125,6 +198,12 @@ const Tiptap = () => {
               ${editor.isActive('bulletList') ? 'bg-stone-200' : 'bg-white'}`}
             >
               <IoIosQuote className={'fill-stone-600'} />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().setHorizontalRule().run()}
+              className={`h-full w-7 cursor-pointer rounded-md border border-solid border-stone-500 fill-stone-600 px-1`}
+            >
+              <RiSeparator className={'fill-stone-600'} />
             </button>
             <button
               onClick={() => editor.chain().focus().toggleCodeBlock().run()}
